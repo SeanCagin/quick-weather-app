f9b8911a36b4da3c220f7cffd88e18fbe98f5905
Sean Uz <cuz3@gatech.edu>
2024-09-14 21:32:20 -0400

    Finished.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ const FIRST_HALF =
 
 const search = document.querySelector("button#search");
 const input = document.querySelector("input#search-query");
-const display = document.querySelector("div#temp");
+const displayF = document.querySelector("div#tempF");
+const displayC = document.querySelector("div#tempC");
 
 search.addEventListener("click", async () => {
   try {
     let data = await fetch(FIRST_HALF + input.value + API_EXTENSION);
     data = await data.json();
-    display.textContent = data.currentConditions.temp;
+    console.log(data);
+    let tempF = data.currentConditions.temp;
+    let tempC = (((tempF - 32) / 9) * 5).toFixed(1);
+    displayF.textContent = `Temp in F: ${tempF}`;
+    displayC.textContent = `Temp in C: ${tempC}`;
   } catch (e) {
     alert(`Either the weather API is down or you entered an invalid 
       search query`);
